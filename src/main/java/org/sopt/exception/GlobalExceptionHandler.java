c3e50abe716d407e5fd2ea42160cbfb34c9324e9
dongchan0105 <come753zz@gmail.com>
2025-11-04 22:30:47 +0900

    ✅ GlobalHandler를 도입해서 try catch 반복을 줄이고 공통으로 처리 할 수 있도록 변경

---
+++ src/main/java/org/sopt/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package org.sopt.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DomainException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFound(DomainException e) {
+        return Map.of("message", e.getMessage());
+    }
+
+    @ExceptionHandler( MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleBadRequest(Exception e) {
+        String msg = (e instanceof MethodArgumentNotValidException manv)
+                ? manv.getBindingResult().getAllErrors().get(0).getDefaultMessage()
+                : e.getMessage();
+        return Map.of("message", msg);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Map<String, String> handleIntegrity (DataIntegrityViolationException e){
+        return Map.of("message", "데이터 제약 조건 위반(중복 제목 등)");
+    }
+}
